dc4fd82e11691eb9c23af4b202dedbff101e1a70
vimalvyshagam <vimalvyshagam@gmail.com>
2020-03-29 21:41:56 +0530

    in server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
+
+// server static asset if we'er in production.
+if (process.env.NODE_ENV === "production") {
+  // set static folder.
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started at ${port}`));
